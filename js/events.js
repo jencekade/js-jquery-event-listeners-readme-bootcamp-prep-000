5aa66342359f793f8542b1fb119e3d0057058952
jencekade <gaephuoc@gmail.com>
2017-12-26 08:41:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,18 +1,28 @@
 //define functions here
 function getIt() {
-  
+  $("p").on("click", function (){
+    alert(`Hey!`);
+  });
 }
 
 function frameIt() {
-  
+  $("img").on("load", function(){
+    
+  });
 }
 
 function pressIt() {
-  
+  $("#typing").on("keydown", function() {
+    if(key.which == 47) {
+      alert(`G was pressed.`);
+    }
+  });
 }
 
 function submitIt() {
-  
+  $("form").on("submit", function(){
+    alert(`Your from is going to be submitted now.`);
+  });
 }
 
 $(document).ready(function(){
